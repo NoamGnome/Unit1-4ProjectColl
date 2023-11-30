821b985d53086a6c7871743f565554a4b1c158b5
NoamGnome <146112021+NoamGnome@users.noreply.github.com>
2023-11-30 12:35:26 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,27 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         EquationClass eq = new EquationClass();
         level1 l1 = new level1();
+        int correct = 0;
+        int level1Streak = 0;
         System.out.println("|-----------------------|");
         System.out.println("|Welcome to LifeEquation|");
         System.out.println("|-----------------------|");
         System.out.println("|--------------------------------------------|");
         System.out.println("|Your name is your health!                   |");
         System.out.println("|You lose a letter for each incorrect answer!|");
-        System.out.println("|It gets harder as you get them right!       |");
+        System.out.println("|once you get three right, it gets harder!   |");
         System.out.println("|--------------------------------------------|");
         System.out.print("Please enter your name: ");
         String name = scan.nextLine();
+        String originalName = eq.getName();
         eq.setName(name);
         System.out.println();
         System.out.println("Nice to meet you " + eq.getName() + "!");
         System.out.println("Player stats: ");
         System.out.println("Health bar: " + eq.getName());
-        System.out.println("Equations correct: 0");
         int life = eq.getName().length();
         while (life != 0) {
+            System.out.println("Equations correct: " + correct);
             int random = l1.random();
             System.out.println(l1.chooser(random));
             System.out.print("What is x: ");
@@ -33,13 +36,20 @@ public class Main {
                 eq.addLetter(letter);
                 System.out.println(eq.getName());
                 life++;
+                correct++;
+                level1Streak++;
             }
             if (!l1.correct) {
                 eq.removeLetter();
                 System.out.println("NEW NAME: " + eq.getName());
                 life--;
             }
+            if (level1Streak == 3) {
+                break;
+            }
+
         }
-        System.out.print("GAME OVER");
+        System.out.println("GAME OVER!");
+        System.out.print("Thanks for playing " + name);
     }
 }
