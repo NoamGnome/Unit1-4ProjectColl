f05f18d8e34bb3210e66d578608ee7b31f4a2838
NoamGnome <146112021+NoamGnome@users.noreply.github.com>
2023-12-04 05:09:35 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,58 +4,131 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         Name eq = new Name();
         Level1 l1 = new Level1();
-        int correct = 0;
-        int level1Streak = 0;
-        printIntro();
-        String name = scan.nextLine();
-        String originalName = eq.getName();
-        eq.setName(name);
+        Level2 l2 = new Level2();
+        Level3 l3 = new Level3();
         l1.populate();
+        l2.populate();
+        l3.populate();
+        int addLetter = 0;
+        int correct = 0;
+        System.out.println("|-----------------------|");
+        System.out.println("|Welcome to LifeEquation|");
+        System.out.println("|-----------------------|");
+        System.out.println("|--------------------------------------------|");
+        System.out.println("|Your name is your health, max health is 8!  |");
+        System.out.println("|You may add up to 3 letters to your name    |");
+        System.out.println("|You lose a letter for each incorrect answer!|");
+        System.out.println("|once you get three right, it gets harder!   |");
+        System.out.println("|Get 8 points and you win!                   |");
+        System.out.println("|--------------------------------------------|");
+        System.out.print("Please enter your name: ");
+        String name = scan.nextLine();
+        if (name.length() < 9) {
+            eq.setName(name);
+        }
+        while (!(name.length() < 9)) {
+            System.out.print("Enter a name 8 characters or less: ");
+            name = scan.nextLine();
+            eq.setName(name);
+        }
         System.out.println();
         System.out.println("Nice to meet you " + eq.getName() + "!");
         System.out.println("Player stats: ");
         System.out.println("Health bar: " + eq.getName());
         int life = eq.getName().length();
         while (life != 0) {
-            life = eq.getName().length();
-            System.out.println("Equations correct: " + correct);
-            int random = l1.random();
-            System.out.println(l1.chooser(random));
-            System.out.print("What is x: ");
-            String answer = scan.nextLine();
-            l1.checkAnswer(answer);
-            if (l1.correct) {
-                System.out.print("Please enter a letter to your name: ");
-                String letter = scan.nextLine();
-                eq.addLetter(letter);
-                System.out.println(eq.getName());
-                life++;
-                correct++;
-                level1Streak++;
+            if (correct < 3) {
+                life = eq.getName().length();
+                System.out.println("points: " + correct);
+                int random1 = l1.random();
+                System.out.println(l1.chooser(random1));
+                System.out.print("What is x: ");
+                String answer1 = scan.nextLine();
+                l1.checkAnswer(answer1);
+                if (l1.correct) {
+                    if (addLetter != 3 && name.length() < 8) {
+                        System.out.print("Please enter a letter to your name: ");
+                        String letter = scan.nextLine();
+                        eq.addLetter(letter);
+                        addLetter++;
+                    }
+                    System.out.println("Health: " + eq.getName());
+                    life++;
+                    correct++;
+                }
+                if (!l1.correct) {
+                    eq.removeLetter();
+                    System.out.println("Health: " + eq.getName());
+                    life--;
+                    correct--;
+                }
             }
-            if (!l1.correct) {
-                eq.removeLetter();
-                System.out.println("NEW NAME: " + eq.getName());
-                life--;
+            if (correct < 6 && correct > 2) {
+                life = eq.getName().length();
+                System.out.println("points: " + correct);
+                int random2 = l2.random();
+                System.out.println(l2.chooser(random2));
+                System.out.print("What is x: ");
+                String answer2 = scan.nextLine();
+                l2.checkAnswer(answer2);
+                if (l2.correct) {
+                    if (addLetter != 3 && name.length() < 8) {
+                        System.out.print("Please enter a letter to your name: ");
+                        String letter = scan.nextLine();
+                        eq.addLetter(letter);
+                        addLetter++;
+                    }
+                    System.out.println("Health: " + eq.getName());
+                    life++;
+                    correct++;
+                }
+                if (!l2.correct) {
+                    eq.removeLetter();
+                    System.out.println("Health: " + eq.getName());
+                    life--;
+                    correct--;
+                }
             }
-            if (level1Streak == 3) {
+            if (correct < 9 && correct > 5) {
+                life = eq.getName().length();
+                System.out.println("points: " + correct);
+                int random = l3.random();
+                System.out.println(l3.chooser(random));
+                System.out.print("What is x: ");
+                String answer = scan.nextLine();
+                l3.checkAnswer(answer);
+                if (l3.correct) {
+                    if (addLetter != 3 && name.length() < 8) {
+                        System.out.print("Please enter a letter to your name: ");
+                        String letter = scan.nextLine();
+                        eq.addLetter(letter);
+                        addLetter++;
+                    }
+                    System.out.println("Health: " + eq.getName());
+                    life++;
+                    correct++;
+                }
+                if (!l3.correct) {
+                    eq.removeLetter();
+                    System.out.println("Health: " + eq.getName());
+                    life--;
+                    correct--;
+                }
+            }
+            if (correct == 9) {
                 break;
             }
-
         }
-        System.out.println("GAME OVER!");
-        System.out.print("Thanks for playing " + name);
-    }
-    public static void printIntro()
-    {
-        System.out.println("|-----------------------|");
-        System.out.println("|Welcome to LifeEquation|");
-        System.out.println("|-----------------------|");
-        System.out.println("|--------------------------------------------|");
-        System.out.println("|Your name is your health!                   |");
-        System.out.println("|You lose a letter for each incorrect answer!|");
-        System.out.println("|once you get three right, it gets harder!   |");
-        System.out.println("|--------------------------------------------|");
-        System.out.print("Please enter your name: ");
+        if (correct == 9) {
+            System.out.println("|--------------------------|");
+            System.out.println("Congratulations!");
+            System.out.println("Thanks for playing " + name);
+            System.out.println("|--------------------------|");
+        }
+        if (life == 0) {
+            System.out.println("|--------------------|");
+            System.out.println("|You suck get better!|");
+            System.out.println("|--------------------|");
+        }
     }
 }
