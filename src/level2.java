845122ab368a35ff27a24d177b80b572729865f9
NoamGnome <146112021+NoamGnome@users.noreply.github.com>
2023-12-04 05:08:46 -0500

    Update level2.java

--- src/level2.java
+++ src/level2.java
@@ -1,39 +1,26 @@
-import java.util.Scanner;
 import java.util.ArrayList;
-public class level1 {
-    ArrayList<Integer> equations = new ArrayList<Integer>();
-    Scanner scan = new Scanner(System.in);
-    EquationClass eq = new EquationClass();
-    boolean one = true;
-    boolean two = true;
-    boolean three = true;
-    boolean four = true;
-    boolean five = true;
-    boolean six = true;
-    boolean seven = true;
-    boolean eight = true;
-    boolean nine = true;
-    private int random = 0;
+public class Level2 {
+    ArrayList<Integer> equations = new ArrayList<>();
+    private int random;
     boolean correct = false;
 
+    public void populate()
+    {
+        for(int i = 1; i < 10; i++)
+        {
+            equations.add(i);
+        }
+    }
+    //DOTADIW:  Do one thing, and do it well.
     public int random() {
-        equations.add(1);
-        equations.add(2);
-        equations.add(3);
-        equations.add(4);
-        equations.add(5);
-        equations.add(6);
-        equations.add(7);
-        equations.add(8);
-        equations.add(9);
-        equations.add(10);
-        random = (int) (Math.random() * equations.size());
+        int index = (int) (Math.random() * equations.size());
+        random = equations.get(index);
         return random;
     }
 
     public void checkAnswer(String answer) {
         if (random == 1) {
-            String an1 = "5";
+            String an1 = "3";
             if (answer.equals(an1)) {
                 correct = true;
                 System.out.println("CORRECT! +1");
@@ -43,7 +30,7 @@ public class level1 {
             }
         }
         if (random == 2) {
-            String an2 = "8";
+            String an2 = "6";
             if (answer.equals(an2)) {
                 correct = true;
                 System.out.println("CORRECT! +1");
@@ -53,7 +40,7 @@ public class level1 {
             }
         }
         if (random == 3) {
-            String an3 = "5";
+            String an3 = "9";
             if (answer.equals(an3)) {
                 correct = true;
                 System.out.println("CORRECT! +1");
@@ -63,7 +50,7 @@ public class level1 {
             }
         }
         if (random == 4) {
-            String an4 = "8";
+            String an4 = "4";
             if (answer.equals(an4)) {
                 correct = true;
                 System.out.println("CORRECT! +1");
@@ -73,7 +60,7 @@ public class level1 {
             }
         }
         if (random == 5) {
-            String an5 = "3";
+            String an5 = "17";
             if (answer.equals(an5)) {
                 correct = true;
                 System.out.println("CORRECT! +1");
@@ -83,7 +70,7 @@ public class level1 {
             }
         }
         if (random == 6) {
-            String an6 = "9";
+            String an6 = "8";
             if (answer.equals(an6)) {
                 correct = true;
                 System.out.println("CORRECT! +1");
@@ -93,7 +80,7 @@ public class level1 {
             }
         }
         if (random == 7) {
-            String an7 = "7";
+            String an7 = "1";
             if (answer.equals(an7)) {
                 correct = true;
                 System.out.println("CORRECT! +1");
@@ -103,7 +90,7 @@ public class level1 {
             }
         }
         if (random == 8) {
-            String an8 = "9";
+            String an8 = "2";
             if (answer.equals(an8)) {
                 correct = true;
                 System.out.println("CORRECT! +1");
@@ -113,7 +100,7 @@ public class level1 {
             }
         }
         if (random == 9) {
-            String an9 = "8";
+            String an9 = "6";
             if (answer.equals(an9)) {
                 correct = true;
                 System.out.println("CORRECT! +1");
@@ -125,50 +112,41 @@ public class level1 {
     }
     public String chooser(int random) {
         String eq = "";
-        if (random == 1 && one) {
-            eq = "7 + x = 12";
-            equations.remove(0);
-            one = false;
+        if (random == 1) {
+            eq = "4x + 1 = 2x + 7";
+            equations.remove(Integer.valueOf(1));
         }
-        if (random == 2 && two) {
-            eq = "16 - x = 8";
-            equations.remove(1);
-            two = false;
+        if (random == 2) {
+            eq = "5x + 4 = 3x + 16";
+            equations.remove(Integer.valueOf(2));
         }
-        if (random == 3 && three) {
-            eq = "x / 1 = 5";
-            equations.remove(2);
-            three = false;
+        if (random == 3) {
+            eq = "7x + 1 = 2x + 46";
+            equations.remove(Integer.valueOf(3));
         }
-        if (random == 4 && four) {
-            eq = "x + 9 = 17";
-            equations.remove(3);
-            four = false;
+        if (random == 4) {
+            eq = "6x - 3 = 2x + 13";
+            equations.remove(Integer.valueOf(4));
         }
-        if (random == 5 && five) {
-            eq = "x * 6 = 18";
-            equations.remove(4);
-            five = false;
+        if (random == 5) {
+            eq = "9x - 10 = 7x + 24";
+            equations.remove(Integer.valueOf(5));
         }
-        if (random == 6 && six) {
-            eq = "36 / x = 4";
-            equations.remove(5);
-            six = false;
+        if (random == 6) {
+            eq = "2x + 21 = 4x + 5";
+            equations.remove(Integer.valueOf(6));
         }
-        if (random == 7 && seven) {
-            eq = "x + 6 = 13";
-            equations.remove(6);
-            seven = false;
+        if (random == 7) {
+            eq = "x + 2 = 5x - 2";
+            equations.remove(Integer.valueOf(7));
         }
-        if (random == 8 && eight) {
-            eq = "18 - x = 9";
-            equations.remove(7);
-            eight = false;
+        if (random == 8) {
+            eq = "5x + 2 = 16 - 2x";
+            equations.remove(Integer.valueOf(8));
         }
-        if (random == 9 && nine) {
-            eq = "15 - x = 7";
-            equations.remove(8);
-            nine = false;
+        if (random == 9) {
+            eq = "3x - 1 = 23 - x";
+            equations.remove(Integer.valueOf(9));
         }
         return eq;
     }
